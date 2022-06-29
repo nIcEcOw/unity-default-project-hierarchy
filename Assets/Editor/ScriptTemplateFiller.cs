c2e1a750b96865b7511611cbd5611ff012d0bedc
bitbucket <bitbucket@gagandeepbali.uk.to>
2022-06-29 08:33:36 +0530

    Code Refactored and comments added

--- Assets/Editor/ScriptTemplateFiller.cs
+++ Assets/Editor/ScriptTemplateFiller.cs
@@ -8,63 +8,65 @@ using UnityEditor;
 using UnityEngine;
 
 public class ScriptTemplateFiller : UnityEditor.AssetModificationProcessor {
-
+  
+  // Delimiters to be used for folder separation
   private static char[] delimiters = new char[] {'/', '\\', '.'};
-  private static List<string> foldersToIgnore = new List<string> {"Code", "Scripts"};
+  
+  // Folders to be ignored 
+  private static List<string> foldersToIgnore = new List<string> {"Assets", "Code", "Script"};
 
   public static void OnWillCreateAsset(string _assetName) {
+    // Ignoring all .meta files
     _assetName = _assetName.Replace(".meta", string.Empty);
-    
     if(!IsFileWithExtension(_assetName, out int index)) return;
-
+    // Check to see if C# script
     if(!IsCSharpFile(_assetName, index)) return;
-
-    string namespaceString = GetNamespaceName(_assetName, out string projectName);
-
-    FillTemplateFields(_assetName, namespaceString, projectName);
-
+    // Create namespace Value
+    string namespaceString = GetNamespaceValue(_assetName);
+    // Fill in the template fields
+    FillTemplateFields(_assetName, namespaceString);
+    // Refresh AssetDatabase
     AssetDatabase.Refresh();
   }
-
+  
+  // Check to test if the assetName has file extension associated with it
   private static bool IsFileWithExtension(string assetName, out int index) {
     index = assetName.LastIndexOf('.');
     return index > -1;
   }
-
+  
+  // Check to test if the assetName belongs to a C# file
   private static bool IsCSharpFile(string assetName, int index) {
     assetName = assetName.Substring(index);
     return assetName.Equals(".cs");
   }
-
-  private static string GetNamespaceName(string assetName, out string projectName) {
+  
+  // Create namespace value
+  private static string GetNamespaceValue(string assetName) {
     string namespaceString = string.Empty;
-    projectName = string.Empty;
 
     List<string> folders = assetName.Split(delimiters).ToList();
     folders = folders.Except(foldersToIgnore).ToList();
 
-    if(folders.Count < 1) {
+    if(folders.Count < 3) {
       namespaceString = "Globals";
       return namespaceString;
+    } else {
+      for(int i = 0; i < folders.Count - 2; ++i) {
+        namespaceString += folders[i];
+        if(i < folders.Count - 3)
+          namespaceString += ".";
+      }
     }
     
-    for(int i = 1; i < folders.Count - 2; ++i) {
-      namespaceString += folders[i];
-      if(i < folders.Count - 3)
-        namespaceString += ".";
-    }
-
-    projectName = folders[1];
-
     return namespaceString;
   }
-
-  private static void FillTemplateFields(string assetName, string namespaceString, string projectName) {
+  
+  // Fill Template Fields
+  private static void FillTemplateFields(string assetName, string namespaceString) {
     string fileContent = System.IO.File.ReadAllText(assetName);
     fileContent = fileContent.Replace("#DATETIME#", System.DateTime.UtcNow.ToLongDateString());
     fileContent = fileContent.Replace("#DEVELOPER#", System.Environment.UserName);
-    fileContent = fileContent.Replace("#PROJECTNAME#", 
-      string.IsNullOrEmpty(namespaceString) ? Application.productName : projectName);
     fileContent = fileContent.Replace("#ROOTNAMESPACE#", namespaceString);
     System.IO.File.WriteAllText(assetName, fileContent);
   }
